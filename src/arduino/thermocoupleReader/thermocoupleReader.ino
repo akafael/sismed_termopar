809bebcd399308fc82a13900269e54218b4767f8
Rafael <raffaellimma@gmail.com>
2016-11-07 09:49:52 -0200

    código ajustado para aquisição com perído fixo de amostragem

--- src/arduino/thermocoupleReader/thermocoupleReader.ino
+++ src/arduino/thermocoupleReader/thermocoupleReader.ino
@@ -2,20 +2,40 @@
  * Medida de Temperatura usando LM35 e Termopar MTK-01
  * 
  * @authors Rafael, Camila, Pamella
- * @version 0.1 'Teste Leitura'
+ * @version 0.12 'Teste Leitura multiplos sensores'
  */
 
 
-#define PIN_LM35 A0
+#define PIN_LM35      A0
+#define PIN_MTK01     A1
+#define INTERVAL      500  //ms
+
+unsigned long lasttime = millis();
 
 void setup() {
   // Inicia Porta Serial
   Serial.begin(9600);
+
+  // Define Portas Sensores
+  pinMode(PIN_LM35,INPUT);
+  pinMode(PIN_MTK01,INPUT);
 }
 
 void loop() {
-  int temp1 = analogRead(PIN_LM35);
-  Serial.println(temp1);
+  // Loop com período fixo de Amostragem
+  if (millis() - lasttime >= INTERVAL) {
+    // Ajusta Timer
+    lasttime = millis();
+    
+    // Ler o valor dos sensores
+    int rawTemp1 = analogRead(PIN_LM35);
+    int rawTemp2 = analogRead(PIN_MTK01);
 
-  delay(100);
-}`
+    // Envia via Serial
+    Serial.print(lasttime);
+    Serial.print(' ');
+    Serial.print(rawTemp1);
+    Serial.print(' ');
+    Serial.println(rawTemp2);
+  }
+}
