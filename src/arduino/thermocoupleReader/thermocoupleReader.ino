23ec7f4080cfc9c834065bc254f1c6f9b65db227
Rafael <raffaellimma@gmail.com>
2016-11-11 11:41:54 -0200

    Filtro de Kalman adicionado

--- src/arduino/thermocoupleReader/thermocoupleReader.ino
+++ src/arduino/thermocoupleReader/thermocoupleReader.ino
@@ -1,8 +1,8 @@
- /**
+/**
  * Medida de Temperatura usando LM35 e Termopar MTK-01
  * 
  * @authors Rafael, Camila, Pamella
- * @version 0.12 'Teste Leitura multiplos sensores'
+ * @version 0.9 'Filtros Implementados'
  */
 
 // Bibliotecas ---------------------------------------------------------------------------------
@@ -14,10 +14,25 @@
 #define PIN_LM35      A0
 #define PIN_MTK01     A1
 
+#define GAIN_LM35      6
+#define GAIN_MTK01     101
+
+#define A_LM35      100*(5/1024)
+#define A_MTK01     410*(5/1024)
+
 #define INTERVAL      200  //ms
 #define SIZE_BUFFER   10
 #define SIZE_BUFFER_F 10   // Parâmentro Filtro Média Móvel
 #define ALPHA         0.8  // Parâmentro Filtro Média Móvel Ponderada
+
+// Parâmetros Filtro de Kalman
+#define KALMAN_A        1
+#define KALMAN_B        0
+#define KALMAN_CONTROL  0
+#define KALMAN_R        0.5
+#define KALMAN_C        1
+#define KALMAN_Q        10
+#define KALMAN_COV      10000
 
 // Variáveis Globais  --------------------------------------------------------------------------
 
@@ -38,8 +53,11 @@ float bufferTemp[SIZE_BUFFER];
 float bufferAvgFilter[SIZE_BUFFER];
 float bufferMvAvgFilter[SIZE_BUFFER];
 float bufferMvAvgExpFilter[SIZE_BUFFER];
+float bufferKalmanFilter[SIZE_BUFFER];
 
 float pMvAvgExpFilter[4]; // Pesos Filtro Média Móvel Ponderada
+
+float kalman_cov = 10000;  // Parâmetro Filtro de Kalman
 
 // Setup ---------------------------------------------------------------------------------
 
@@ -80,29 +98,34 @@ void loop() {
     lasttime = millis();
     
     // Ler o valor dos sensores
-    float rawTemp1 = analogRead(PIN_LM35);
-    float rawTemp2 = analogRead(PIN_MTK01);
+    bufferLM35[k] = analogRead(PIN_LM35);
+    bufferMTK01[k] = analogRead(PIN_MTK01);
+
+    // Calcula a Temperatura
+    float temp1 = bufferLM35[k]*A_LM35;   // Temperatura de Referência ( Ambiente )
+    float temp2 = bufferMTK01[k]*A_MTK01; // Medida da Variação da Temperatura
+    bufferTemp[k] =  temp1 + temp2;       // Compensação por Software
 
     // Filtragem
-    bufferLM35[k] = rawTemp1;
-    bufferMTK01[k] = rawTemp2;
-    
     filterAverenge(bufferLM35,index,k);
     filterMovingAverenge(bufferLM35,k);
     filterMovingAverengeExp(bufferLM35,k);
+    filterKalman(bufferLM35,k);
 
     // Envia via Serial
     Serial.print(lasttime);
     Serial.print(' ');
-    Serial.print(rawTemp1);
+    Serial.print(bufferLM35[k]);
     Serial.print(' ');
-    Serial.print(rawTemp2);
+    Serial.print(bufferMTK01[k]);
     Serial.print(' ');
     Serial.print(bufferAvgFilter[k]);
     Serial.print(' ');
     Serial.print(bufferMvAvgFilter[k]);
     Serial.print(' ');
-    Serial.println(bufferMvAvgExpFilter[k]);
+    Serial.print(bufferMvAvgExpFilter[k]);
+    Serial.print(' ');
+    Serial.println(bufferKalmanFilter[k]);
 
     // Incrementa Contadores
     index = index+1;
@@ -156,3 +179,20 @@ void filterMovingAverengeExp(float buffer[],int k){
   bufferMvAvgExpFilter[k] += buffer[a1]*pMvAvgExpFilter[2];
   bufferMvAvgExpFilter[k] += buffer[k]*pMvAvgExpFilter[3];
 }
+
+/**
+ * Filtro de Kalman
+ * 
+ * @author Rafael Lima
+ */
+ void filterKalman(float buffer[],int k){
+    // Calcula Endereços
+    int a1 = (k>0)?k-1:SIZE_BUFFER_F-1;
+  
+    float m = KALMAN_A*bufferKalmanFilter[a1] + KALMAN_B*KALMAN_CONTROL;
+    float S = KALMAN_A*kalman_cov*KALMAN_A + KALMAN_R;
+    float K = S*KALMAN_C/(KALMAN_C*S*KALMAN_C + KALMAN_Q);
+    
+    bufferKalmanFilter[k] = m + K*(buffer[k] - KALMAN_C*m);
+    kalman_cov = (1 - K*KALMAN_C)*S;
+ }
